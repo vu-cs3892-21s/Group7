c96f8fb56b04849ca3daf38859443570f367a0ed
Tim O'Brien <timothy.c.obrien99@gmail.com>
2021-03-25 18:52:24 -0500

    modified model slightly, added test data
    join with code updates

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,40 +1,38 @@
 "use strict";
 
-import React, {useState} from "react";
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import {SideBar} from "./components/sidebar";
-import {Profile} from "./components/profile";
-import {GameGen} from "./components/create";
-import {GamePage} from "./components/game";
-import {LeadershipBoard} from "./components/leadership";
+import { SideBar } from "./components/sidebar";
+import { Profile } from "./components/profile";
+import { GameGen } from "./components/create";
+import { GamePage } from "./components/game";
+import { LeadershipBoard } from "./components/leadership";
 import { HashRouter, Route, Redirect } from "react-router-dom";
-import {render} from "react-dom";
-import {CenteredDiv, HeaderWrap} from "./components/shared";
-import {Theme} from "@material-ui/core/styles";
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-
+import { render } from "react-dom";
+import { CenteredDiv, HeaderWrap } from "./components/shared";
+import { Theme } from "@material-ui/core/styles";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        header:{
-            backgroundColor: "#B5CEF3",
-            minHeight: "100px",
-            margin: "0px",
-            width: "100vw",
-            position: "fixed",
-        },
-    }),
+  createStyles({
+    header: {
+      backgroundColor: "#B5CEF3",
+      minHeight: "100px",
+      margin: "0px",
+      width: "100vw",
+      position: "fixed",
+    },
+  })
 );
 
 const landingStyleTop = {
-    lineHeight: "40vh",
-    fontSize: "60px",
-    fontFamily: "neuemachina-ultrabold",
-    color: "white",
-    width: "100vw",
-}
-
+  lineHeight: "40vh",
+  fontSize: "60px",
+  fontFamily: "neuemachina-ultrabold",
+  color: "white",
+  width: "100vw",
+};
 
 const GridBase = styled.div`
   positive: relative;
@@ -51,100 +49,113 @@ const GridBase = styled.div`
 `;
 
 const defaultUser = {
-    name: "",
-    primary_email: "",
+  name: "",
+  primary_email: "",
 };
 
 const App = () => {
-    // If the user has logged in, grab info from sessionStorage
-    const data = localStorage.getItem("user");
-    const [state, setState] = useState(data ? JSON.parse(data) : defaultUser);
+  // If the user has logged in, grab info from sessionStorage
+  const data = localStorage.getItem("user");
+  const [state, setState] = useState(data ? JSON.parse(data) : defaultUser);
 
-    // Helper to check if the user is logged in or not
-    const loggedIn = () => {
-        return state.primary_email;
-    };
+  // Helper to check if the user is logged in or not
+  const loggedIn = () => {
+    return state.primary_email;
+  };
 
-    const logIn = async (ev: { preventDefault: () => void; target: { offsetParent: {id: string}}; }) => {
-        ev.preventDefault();
-        const endpoint = `http://localhost:5000/login/${ev.target.offsetParent.id}`;
-        try {
-            window.location.href = endpoint
-            const user = {
-                // username: data.login,
-                name: "",
-                primary_email: "",
-                color: "",
-            }
-            localStorage.setItem("user", JSON.stringify(user));
-            setState(user);
-        } catch (err) {
-            alert("An unexpected error occurred.");
-            logOut();
-        }
-    };
-    
-    const onLoggedIn = () => {
-        fetch("/api/v1/session/profile")
-            .then(res => res.json())
-            .then(data => {
-                setState(data);
-                console.log("fetched profile info");
-                console.log(data);
-            })
-            .catch(err => alert(err))
+  const logIn = async (ev: {
+    preventDefault: () => void;
+    target: { offsetParent: { id: string } };
+  }) => {
+    ev.preventDefault();
+    const endpoint = `http://localhost:5000/login/${ev.target.offsetParent.id}`;
+    try {
+      window.location.href = endpoint;
+      const user = {
+        // username: data.login,
+        name: "",
+        primary_email: "",
+        color: "",
+      };
+      localStorage.setItem("user", JSON.stringify(user));
+      setState(user);
+    } catch (err) {
+      alert("An unexpected error occurred.");
+      logOut();
     }
+  };
 
-    // Helper for when a user logs out
-    const logOut = () => {
-        // Wipe localStorage
-        localStorage.removeItem("user");
-        // Reset user state
-        setState(defaultUser);
-    };
+  const onLoggedIn = () => {
+    fetch("/api/v1/session/profile")
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data);
+        console.log("fetched profile info");
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
 
-    const classes = useStyles();
+  // Helper for when a user logs out
+  const logOut = () => {
+    // Wipe localStorage
+    localStorage.removeItem("user");
+    // Reset user state
+    setState(defaultUser);
+  };
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return (
-        <HashRouter>
-        <GridBase>
-            <SideBar loggedIn = {loggedIn()} logIn={logIn} logOut={logOut} username ={state.username}/>
-            <Route exact path="/" component={Landing} />
-            <Route
-                path="/profile"
-                render = {p => {return <Profile currentUser = {state} onLoggedIn={onLoggedIn}/>}}
-            />
-            <Route
-                path="/create"
-                render={p => {
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
-                    return <GameGen {...p.history} />;
-                }}
-            />
-            <Route
-                path="/game/:id"
-                render={p => {return <GamePage {...p}/>;
-                }}
-            />
-            <Route
-                path="/leadership"
-                render={p => <LeadershipBoard currentUser = {state.username} />}
-            />
-        </GridBase>
-        </HashRouter>
-    );
+  const classes = useStyles();
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return (
+    <HashRouter>
+      <GridBase>
+        <SideBar
+          loggedIn={loggedIn()}
+          logIn={logIn}
+          logOut={logOut}
+          username={state.username}
+        />
+        <Route exact path="/" component={Landing} />
+        <Route
+          path="/profile"
+          render={(p) => {
+            return <Profile currentUser={state} onLoggedIn={onLoggedIn} />;
+          }}
+        />
+        <Route
+          path="/create"
+          render={(p) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            return <GameGen {...p.history} />;
+          }}
+        />
+        <Route
+          path="/game/:id"
+          render={(p) => {
+            return <GamePage {...p} />;
+          }}
+        />
+        <Route
+          path="/leadership"
+          render={(p) => <LeadershipBoard currentUser={state.username} />}
+        />
+      </GridBase>
+    </HashRouter>
+  );
 };
 
 const Landing = () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return <div>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <CenteredDiv style = {landingStyleTop}>TRAIN YOUR BRAIN</CenteredDiv>
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return (
+    <div>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      <CenteredDiv style={landingStyleTop}>TRAIN YOUR BRAIN</CenteredDiv>
     </div>
+  );
 };
 
 render(<App />, document.getElementById("root"));
