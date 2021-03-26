d105120a0b741f0161428dc56729a241b48d87b1
Tim O'Brien <timothy.c.obrien99@gmail.com>
2021-03-25 20:32:52 -0500

    client side fix

--- client/src/components/shared.tsx
+++ client/src/components/shared.tsx
@@ -1,10 +1,9 @@
+"use strict";
 
-'use strict';
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 const ErrorBase = styled.div`
   font-family: Revalia;
@@ -14,99 +13,103 @@ const ErrorBase = styled.div`
   min-height: 1.2em;
 `;
 
-export const ErrorMessage = ({ msg = '', hide = false }) => {
-    return (
-        <ErrorBase style={{ display: hide ? 'none' : 'inherit' }}>{msg}</ErrorBase>
-    );
+export const ErrorMessage = ({ msg = "", hide = false }) => {
+  return (
+    <ErrorBase style={{ display: hide ? "none" : "inherit" }}>{msg}</ErrorBase>
+  );
 };
 
 ErrorMessage.propTypes = {
-    msg: PropTypes.string,
-    hide: PropTypes.bool
+  msg: PropTypes.string,
+  hide: PropTypes.bool,
 };
 
 export const useSharedStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        toolbar: theme.mixins.toolbar,
-        labelFormat: {
-            fontSize: "24px",
-            lineHeight: "30px",
-            alignItems: "center",
-            color: "#000000",
-            paddingRight: "1em",
-        },
-        inputFormat: {
-            paddingLeft: "5px",
-            color: "black",
-        },
-        centeredFormat: {
-            width: "50%",
-            height: "50%",
-            overflow: "auto",
-            margin: "auto",
-            textAlign: "center",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-        },
-        buttonFormat: {
-            maxWidth: "200px",
-            minWidth: "150px",
-            maxHeight: "2em",
-            background: "#00538f",
-            lineHeight: "2em",
-            fontSize: "25px",
-            color: "white",
-        },
-        tr: {
-            '&:hover': {
-                background: "#B5CEF3",
-            },
-        },
-
-    }),
+  createStyles({
+    toolbar: theme.mixins.toolbar,
+    labelFormat: {
+      fontSize: "24px",
+      lineHeight: "30px",
+      alignItems: "center",
+      color: "#000000",
+      paddingRight: "1em",
+    },
+    inputFormat: {
+      paddingLeft: "5px",
+      color: "black",
+    },
+    centeredFormat: {
+      width: "50%",
+      height: "50%",
+      overflow: "auto",
+      margin: "auto",
+      textAlign: "center",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    },
+    buttonFormat: {
+      maxWidth: "200px",
+      minWidth: "150px",
+      maxHeight: "2em",
+      background: "#00538f",
+      lineHeight: "2em",
+      fontSize: "25px",
+      color: "white",
+    },
+    tr: {
+      "&:hover": {
+        background: "#B5CEF3",
+      },
+    },
+  })
 );
 
 export const CenteredDiv = styled.div`
-    width: 100%;
-    height: 50%;
-    overflow: auto;
-    margin: auto;
-    text-align: center;
-    position: absolute;
-    top: 0; left: 0; bottom: 0; right: 0;
+  width: 100%;
+  height: 50%;
+  overflow: auto;
+  margin: auto;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 `;
 
 export const AnswerLabel = styled.label`
-    font-size: 24px;
-    line-height: 30px;
-    text-align: right;
-    color: #000000;
-    padding-right: 1em;
+  font-size: 24px;
+  line-height: 30px;
+  text-align: right;
+  color: #000000;
+  padding-right: 1em;
 `;
 
-
 export const AnswerInput = styled.input`
-    padding-left: 5px;
-    color: black;
-    width: 50%;
-    height: 70%;
-    overflow: scroll;
-    margin: auto;
-    position: absolute;
-    top: 0; left: 0; bottom: 0; right: 0;
-
+  padding-left: 5px;
+  color: black;
+  width: 50%;
+  height: 70%;
+  overflow: scroll;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
 
 export const HeaderWrap = styled.div`
-    grid-area: top;
-    padding: 0px 10px;
-    font-size: 48px;
-    line-height: 60px;
-    color: #B5CEF3;
-    padding-left: 30px;
-    padding-top: 20px;
+  grid-area: top;
+  padding: 0px 10px;
+  font-size: 48px;
+  line-height: 60px;
+  color: #b5cef3;
+  padding-left: 30px;
+  padding-top: 20px;
 `;
 
 export const Button = styled.button`
@@ -124,7 +127,7 @@ export const FunButton = styled.button`
   max-width: 300px;
   min-width: 150px;
   max-height: 2em;
-  background:#00538f;
+  background: #00538f;
   border-radius: 5px;
   font-size: 20px;
   color: white;
@@ -133,12 +136,12 @@ export const FunButton = styled.button`
   overflow: auto;
   box-shadow: 0 9px #999;
   &:hover {
-    background-color: #B5CEF3;
+    background-color: #b5cef3;
     box-shadow: 0 5px #666;
     transform: translateY(4px);
   }
-  &:active{
-    background-color: #B5CEF3;
+  &:active {
+    background-color: #b5cef3;
   }
 `;
 
@@ -146,7 +149,7 @@ export const CenteredButton = styled.button`
   max-width: 300px;
   min-width: 200px;
   max-height: 2em;
-  background:#00538f;
+  background: #00538f;
   border-radius: 5px;
   font-size: 25px;
   color: white;
@@ -155,34 +158,19 @@ export const CenteredButton = styled.button`
   overflow: auto;
   margin: auto;
   position: absolute;
-  top: 0; left: 0; bottom: 0; right: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
   box-shadow: 0 9px #999;
   &:hover {
-    background-color: #B5CEF3;
+    background-color: #b5cef3;
     box-shadow: 0 5px #666;
     transform: translateY(4px);
   }
-  &:active{
-    background-color: #B5CEF3;
+  &:active {
+    background-color: #b5cef3;
   }
-`;
-
-export const CenteredButton = styled.button`
-  max-width: 200px;
-  min-width: 150px;
-  max-height: 2em;
-  background:#00538f;
-  border-radius: 5px;
-  line-height: 2em;
-  font-size: 25px;
-  color: white;
-  font-family: revalia;
-  width: 50%;
-  height: 50%;
-  overflow: auto;
-  margin: auto;
-  position: absolute;
-  top: 0; left: 0; bottom: 0; right: 0;
 `;
 
 export const InfoBlock = styled.div`
@@ -190,7 +178,7 @@ export const InfoBlock = styled.div`
   color: black;
   grid-template-columns: auto 1fr;
   grid-template-rows: auto;
-  grid-template-areas: 'labels info';
+  grid-template-areas: "labels info";
 `;
 
 export const InfoData = styled.div`
