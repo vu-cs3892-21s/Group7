fc75007129198ec54c7981b4a97f1aaf00c7a141
Tim O'Brien <timothy.c.obrien99@gmail.com>
2021-04-08 16:00:50 -0500

    fixed some bugs with user stats

--- server/src/db/models/game.py
+++ server/src/db/models/game.py
@@ -34,10 +34,11 @@ class GameQuestion(db.Model):
     def as_dict(self) -> Dict[str, any]:
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+
 class StatsTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     player_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
-    mode = db.Column(db.String(40) index=True)
+    mode = db.Column(db.String(40))
     num_questions = db.Column(db.Integer, default=0)
     num_correct = db.Column(db.Integer, default=0)
     num_games = db.Column(db.Integer, default=0)
--- server/src/db/models/user.py
+++ server/src/db/models/user.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship
 from ..database import db
 
 UserRecord = Dict[str, str]
@@ -9,11 +10,10 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     primary_email = db.Column(db.String(120), index=True, unique=True)
     name = db.Column(db.String(40), index=True, default='')
-    stats = db.Column(db.Integer, db.ForeignKey(StatsTable.id), nullable=False)
+    # may want to update with backref for stats, but this is fine for now
 
     def __repr__(self) -> str:
         return '<User %r>' % self.name
 
     def as_dict(self) -> UserRecord:
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
