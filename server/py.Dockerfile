fa0745607e87a5022ac969abb4b2b3b8a83d8703
Tim O'Brien <timothy.c.obrien99@gmail.com>
2021-04-28 21:00:09 -0500

    testing socket

--- server/py.Dockerfile
+++ server/py.Dockerfile
@@ -10,7 +10,7 @@ ENV FLASK_APP=src/main.py
 ENV FLASK_RUN_HOST=${HOST:-0.0.0.0}
 ENV FLASK_RUN_PORT=${PORT:-5000}
 ENV FLASK_ENV=${FLASK_ENV:-production}
-# ENV DATABASE_URI=${DATABASE_URI:-postgres+pg8000://postgres:postgres@postgres?unix_sock=/cloudsql/seventh-atom-311914:us-central1:postgres-db/.s.PGSQL.5432}
+ENV DATABASE_URI=${DATABASE_URI:-postgresql://postgres:postgres@35.224.144.168/postgres?host=/cloudsql/seventh-atom-311914:us-central1:postgres-db}
 ENV DB_USER=${DB_USER:-postgres}
 ENV DB_PASS=${DB_PASS:-postgres}
 ENV DB_NAME=${DB_NAME:-postgres}
--- server/src/main.py
+++ server/src/main.py
@@ -41,76 +41,82 @@ class Config(object):
 
 app.config.from_object(Config)
 
+print(Config.SQLALCHEMY_DATABASE_URI, Config.FLASK_ENV)
 
-if Config.FLASK_ENV == 'development':
-    db.init_app(app)
-else:
-    def init_connection_engine():
-        db_config = {
-            # [START cloud_sql_postgres_sqlalchemy_limit]
-            # Pool size is the maximum number of permanent connections to keep.
-            "pool_size": 5,
-            # Temporarily exceeds the set pool_size if no connections are available.
-            "max_overflow": 2,
-            # The total number of concurrent connections for your application will be
-            # a total of pool_size and max_overflow.
-            # [END cloud_sql_postgres_sqlalchemy_limit]
+db.init_app(app)
 
-            # [START cloud_sql_postgres_sqlalchemy_backoff]
-            # SQLAlchemy automatically uses delays between failed connection attempts,
-            # but provides no arguments for configuration.
-            # [END cloud_sql_postgres_sqlalchemy_backoff]
 
-            # [START cloud_sql_postgres_sqlalchemy_timeout]
-            # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-            # new connection from the pool. After the specified amount of time, an
-            # exception will be thrown.
-            "pool_timeout": 30,  # 30 seconds
-            # [END cloud_sql_postgres_sqlalchemy_timeout]
+# if Config.FLASK_ENV == 'development':
+#     db.init_app(app)
 
-            # [START cloud_sql_postgres_sqlalchemy_lifetime]
-            # 'pool_recycle' is the maximum number of seconds a connection can persist.
-            # Connections that live longer than the specified amount of time will be
-            # reestablished
-            "pool_recycle": 1800,  # 30 minutes
-            # [END cloud_sql_postgres_sqlalchemy_lifetime]
-        }
-        return init_unix_connection_engine(db_config)
+# else:
+#     def init_connection_engine():
+#         db_config = {
+#             # [START cloud_sql_postgres_sqlalchemy_limit]
+#             # Pool size is the maximum number of permanent connections to keep.
+#             "pool_size": 5,
+#             # Temporarily exceeds the set pool_size if no connections are available.
+#             "max_overflow": 2,
+#             # The total number of concurrent connections for your application will be
+#             # a total of pool_size and max_overflow.
+#             # [END cloud_sql_postgres_sqlalchemy_limit]
 
-    def init_unix_connection_engine(db_config):
-        # [START cloud_sql_postgres_sqlalchemy_create_socket]
-        # Remember - storing secrets in plaintext is potentially unsafe. Consider using
-        # something like https://cloud.google.com/secret-manager/docs/overview to help keep
-        # secrets secret.
-        db_user = os.environ["DB_USER"]
-        db_pass = os.environ["DB_PASS"]
-        db_name = os.environ["DB_NAME"]
-        db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-        cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+#             # [START cloud_sql_postgres_sqlalchemy_backoff]
+#             # SQLAlchemy automatically uses delays between failed connection attempts,
+#             # but provides no arguments for configuration.
+#             # [END cloud_sql_postgres_sqlalchemy_backoff]
 
-        pool = sqlalchemy.create_engine(
+#             # [START cloud_sql_postgres_sqlalchemy_timeout]
+#             # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+#             # new connection from the pool. After the specified amount of time, an
+#             # exception will be thrown.
+#             "pool_timeout": 30,  # 30 seconds
+#             # [END cloud_sql_postgres_sqlalchemy_timeout]
 
-            # Equivalent URL:
-            # postgres+pg8000://<db_user>:<db_pass>@/<db_name>
-            #                         ?unix_sock=<socket_path>/<cloud_sql_instance_name>/.s.PGSQL.5432
-            sqlalchemy.engine.url.URL(
-                drivername="postgresql+pg8000",
-                username=db_user,  # e.g. "my-database-user"
-                password=db_pass,  # e.g. "my-database-password"
-                database=db_name,  # e.g. "my-database-name"
-                query={
-                    "unix_sock": "{}/{}/.s.PGSQL.5432".format(
-                        db_socket_dir,  # e.g. "/cloudsql"
-                        cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-                }
-            ),
-            **db_config
-        )
-        # [END cloud_sql_postgres_sqlalchemy_create_socket]
-        pool.dialect.description_encoding = None
-        return pool
+#             # [START cloud_sql_postgres_sqlalchemy_lifetime]
+#             # 'pool_recycle' is the maximum number of seconds a connection can persist.
+#             # Connections that live longer than the specified amount of time will be
+#             # reestablished
+#             "pool_recycle": 1800,  # 30 minutes
+#             # [END cloud_sql_postgres_sqlalchemy_lifetime]
+#         }
+#         return init_unix_connection_engine(db_config)
 
-    db = init_connection_engine()
+#     def init_unix_connection_engine(db_config):
+#         # [START cloud_sql_postgres_sqlalchemy_create_socket]
+#         # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+#         # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+#         # secrets secret.
+#         db_user = os.environ["DB_USER"]
+#         db_pass = os.environ["DB_PASS"]
+#         db_name = os.environ["DB_NAME"]
+#         db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+#         cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+#         pool = sqlalchemy.create_engine(
+
+#             # Equivalent URL:
+#             # postgres+pg8000://<db_user>:<db_pass>@/<db_name>
+#             #                         ?unix_sock=<socket_path>/<cloud_sql_instance_name>/.s.PGSQL.5432
+#             sqlalchemy.engine.url.URL(
+#                 drivername="postgresql+pg8000",
+#                 username=db_user,  # e.g. "my-database-user"
+#                 password=db_pass,  # e.g. "my-database-password"
+#                 database=db_name,  # e.g. "my-database-name"
+#                 query={
+#                     "unix_sock": "{}/{}/.s.PGSQL.5432".format(
+#                         db_socket_dir,  # e.g. "/cloudsql"
+#                         cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+#                 }
+#             ),
+#             **db_config
+#         )
+#         # [END cloud_sql_postgres_sqlalchemy_create_socket]
+#         pool.dialect.description_encoding = None
+#         return pool
+
+#     db = init_connection_engine()
+#     db = db.connect()
 
 
 login_manager = LoginManager(app)
@@ -189,12 +195,12 @@ def create_test_data() -> None:
 
 
 with app.app_context():
-    drop_everything()
+    # drop_everything()
     db.create_all()
-    create_test_data()
+    # create_test_data()
 
     # loading questions takes a few minutes so we only want to load once
-    load_questions()
+    # load_questions()
 
 
 socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
